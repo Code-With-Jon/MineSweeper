0b4def100f6912ece29c029006c766ac51c42efc
Jon <jon@Sciences-MacBook-Pro.local>
2019-10-30 10:23:40 -0700

    modify recursive call

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+
 //CONSTANTS
 let gridWidth = 20;
 let gridHeight = 20;
@@ -5,7 +7,7 @@ let gridHeight = 20;
 //CASHED
 let bodyEl = document.querySelector('#grid-container');
 let gridEl = document.querySelector('body')
-let playAgain = document.getElementById('reset');
+let playAgain = document.querySelector('#reset');
 let currentCell;
 let cellEl;
 let cells = {
@@ -20,6 +22,12 @@ let cells = {
 }
 let edge = false;
 let counter = 1;
+let bombs = [];
+let leftEdge = false;
+let rightEdge = false;
+let topEdge = false;
+let bottomEdge = false;
+
 for (let i = 0; i < gridHeight; i++) {
     for (let j = 0; j < gridWidth; j++) {
         cellEl = document.createElement('div');
@@ -31,8 +39,20 @@ for (let i = 0; i < gridHeight; i++) {
 }
 
 function init() {
+
     bodyEl.style.visibility = 'visible'
     gridEl.style.background = 'white';
+    cells = {
+        topLeft: 0,
+        topMid: 0,
+        topRight: 0,
+        leftCenter: 0,
+        rightCenter: 0,
+        bottomLeft: 0,
+        bottomMid: 0,
+        bottomRight: 0
+    }
+
 
 }
 
@@ -51,35 +71,61 @@ function recursiveSearch(domObject) {
         if (bombs.includes(cells[prop])) {
             bombCounter += 1;
         }
-
     }
-
     domObject.textContent = bombCounter;
     domObject.style.backgroundColor = 'rgb(30, 75, 77)'
     domObject.style.color = 'white';
     domObject.style.textAlign = 'center';
+
     console.log(bombCounter);
-    if (bombCounter === 0 && (parseInt(domObject.id) + 20) < 400) {
-        console.log('SHOW CELL');
-        console.log(document.getElementById(`${parseInt(domObject.id) + 20}`));
-        recursiveSearch(document.getElementById(`${parseInt(domObject.id) + 19}`));
-        recursiveSearch(document.getElementById(`${parseInt(domObject.id) + 20}`));
-        recursiveSearch(document.getElementById(`${parseInt(domObject.id) + 21}`));
-    }
-    if (bombCounter === 0 && (parseInt(domObject.id) - 20) < 1) {
-        recursiveSearch(document.getElementById(`${parseInt(domObject.id) - 19}`));
-        recursiveSearch(document.getElementById(`${parseInt(domObject.id) - 20}`));
-        recursiveSearch(document.getElementById(`${parseInt(domObject.id) - 21}`));
-    }
-    if (bombCounter === 0 && (parseInt(domObject.id) % 20 === 0)) {
-        recursiveSearch(document.getElementById(`${parseInt(domObject.id) + 1}`));
-    }
-    if (bombCounter === 0 && (parseInt(domObject.id) - 1) % 20 === 0) {
-        recursiveSearch(document.getElementById(`${parseInt(domObject.id) - 1}`));
-    }
-    else {
+
+    if (bombCounter !== 0 && domObject === 'active') {
         return;
+    } else {
+        console.log('HITTTTTTT')
+        if (bombCounter === 0 && bottomEdge === false && domObject !== 'active') {
+            recursiveSearch(document.getElementById(`${parseInt(domObject.id) + 20}`));
+            domObject.classList.add('active');
+            recursiveSearch(document.getElementById(`${parseInt(domObject.id) + 21}`));
+            domObject.classList.add('active');
+            recursiveSearch(document.getElementById(`${parseInt(domObject.id) + 19}`));
+
+        }
+        if (bombCounter === 0 && topEdge === false && domObject !== 'active') {
+            recursiveSearch(document.getElementById(`${parseInt(domObject.id) - 20}`));
+            domObject.classList.add('active');
+
+            recursiveSearch(document.getElementById(`${parseInt(domObject.id) - 21}`));
+            domObject.classList.add('active');
+
+            recursiveSearch(document.getElementById(`${parseInt(domObject.id) - 19}`));
+            domObject.classList.add('active');
+        }
+        if (bombCounter === 0 && rightEdge === false && domObject !== 'active') {
+            recursiveSearch(document.getElementById(`${parseInt(domObject.id) + 1}`));
+            domObject.classList.add('active');
+        }
+        if (bombCounter === 0 && leftEdge === false && domObject !== 'active') {
+            recursiveSearch(document.getElementById(`${parseInt(domObject.id) - 1}`));
+            domObject.classList.add('active');
+        }
     }
+
+
+
+    // if ((bombCounter === 0 &&
+    //     (parseInt(domObject.id) + 20) < 400 &&
+    //     (parseInt(domObject.id) - 20) > 1 &&
+    //     (parseInt(domObject.id) % 20 !== 0) &&
+    //     (parseInt(domObject.id) - 1) % 20 !== 0)) {
+    //     console.log('SHOW CELL');
+    //     console.log(document.getElementById(`${parseInt(domObject.id) + 20}`));
+
+
+    // }
+    // else {
+    //     return;
+    // }
 
 }
 
@@ -91,24 +137,32 @@ function checkNeighbors(e) {
 }
 
 function checkEdges(currentCell) {
+
     //left
     if ((parseInt(currentCell.id) - 1) % 20 === 0) {
         console.log('Left Hit');
-        edge = true;
+        leftEdge = true;
     }
+
     //right
+
     if (parseInt(currentCell.id) % 20 === 0) {
         console.log('Right Hit');
-        edge = true;
+        rightEdge = true;
     }
+    //top
+
     if (parseInt(currentCell.id) - 20 < 1) {
         console.log('Top Edge');
-        edge = true
+        topEdge = true
     }
+    //bottom
+
     if (parseInt(currentCell.id) + 20 > 400) {
         console.log('Bottom Edge');
-        edge = true;
+        bottomEdge = true;
     }
+    return leftEdge, rightEdge, topEdge, bottomEdge;
 }
 
 function checkTop(e) {
@@ -150,12 +204,11 @@ function checkMid(e) {
     //console.log(cells.leftCenter, cells.rightCenter);
 }
 
-let bombs = [];
+
 function generateBombs() {
-    for (let i = 0; i <= 50; i++) {
+    for (let i = 0; i <= 20; i++) {
         bombs.push(Math.floor(Math.random() * 400 + 1));
     }
     //  console.log(bombs)
 }
-
 generateBombs();
