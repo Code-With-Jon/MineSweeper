7e38fb649f890b116e63c80b6913b796671e1ec7
Jon <jon@Sciences-MacBook-Pro.local>
2019-10-29 17:02:44 -0700

    Added recursive function - still needs work

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ let bodyEl = document.querySelector('#grid-container');
 let gridEl = document.querySelector('body')
 let playAgain = document.getElementById('reset');
 let currentCell;
-let bombCounter = 0;
+let cellEl;
 let cells = {
     topLeft: 0,
     topMid: 0,
@@ -18,11 +18,11 @@ let cells = {
     bottomMid: 0,
     bottomRight: 0
 }
-
+let edge = false;
 let counter = 1;
 for (let i = 0; i < gridHeight; i++) {
     for (let j = 0; j < gridWidth; j++) {
-        let cellEl = document.createElement('div');
+        cellEl = document.createElement('div');
         bodyEl.appendChild(cellEl);
         cellEl.setAttribute("id", counter);
         counter++;
@@ -33,56 +33,87 @@ for (let i = 0; i < gridHeight; i++) {
 function init() {
     bodyEl.style.visibility = 'visible'
     gridEl.style.background = 'white';
+
 }
 
-
-function checkNeighbors(e) {
-    if (bombs.indexOf(e)) {
+function recursiveSearch(domObject) {
+    let bombCounter = 0;
+    if (bombs.includes(parseInt(domObject.id))) {
         bodyEl.style.visibility = 'hidden'
         gridEl.style.background = 'url("https://upload.wikimedia.org/wikipedia/commons/9/95/Bomb_icon.svg") no-repeat center';
         playAgain.addEventListener('click', init)
     }
-    checkEdges(e.target.id);
-    checkTop(e);
-    checkMid(e)
-    checkBottom(e);
+    checkEdges(domObject);
+    checkTop(domObject);
+    checkMid(domObject)
+    checkBottom(domObject);
     for (let prop in cells) {
         if (bombs.includes(cells[prop])) {
             bombCounter += 1;
         }
+
     }
-    e.target.textContent = bombCounter;
-    e.target.style.backgroundColor = 'rgb(30, 75, 77)'
-    e.target.style.color = 'white';
-    e.target.style.textAlign = 'center';
+
+    domObject.textContent = bombCounter;
+    domObject.style.backgroundColor = 'rgb(30, 75, 77)'
+    domObject.style.color = 'white';
+    domObject.style.textAlign = 'center';
     console.log(bombCounter);
-    bombCounter = 0;
+    if (bombCounter === 0 && (parseInt(domObject.id) + 20) < 400) {
+        console.log('SHOW CELL');
+        console.log(document.getElementById(`${parseInt(domObject.id) + 20}`));
+        recursiveSearch(document.getElementById(`${parseInt(domObject.id) + 19}`));
+        recursiveSearch(document.getElementById(`${parseInt(domObject.id) + 20}`));
+        recursiveSearch(document.getElementById(`${parseInt(domObject.id) + 21}`));
+    }
+    if (bombCounter === 0 && (parseInt(domObject.id) - 20) < 1) {
+        recursiveSearch(document.getElementById(`${parseInt(domObject.id) - 19}`));
+        recursiveSearch(document.getElementById(`${parseInt(domObject.id) - 20}`));
+        recursiveSearch(document.getElementById(`${parseInt(domObject.id) - 21}`));
+    }
+    if (bombCounter === 0 && (parseInt(domObject.id) % 20 === 0)) {
+        recursiveSearch(document.getElementById(`${parseInt(domObject.id) + 1}`));
+    }
+    if (bombCounter === 0 && (parseInt(domObject.id) - 1) % 20 === 0) {
+        recursiveSearch(document.getElementById(`${parseInt(domObject.id) - 1}`));
+    }
+    else {
+        return;
+    }
+
 }
 
-function checkSelf(currentCell) {
+
+
+function checkNeighbors(e) {
+    recursiveSearch(e.target);
 
 }
 
 function checkEdges(currentCell) {
     //left
-    if ((parseInt(currentCell) - 1) % 20 === 0) {
+    if ((parseInt(currentCell.id) - 1) % 20 === 0) {
         console.log('Left Hit');
+        edge = true;
     }
     //right
-    if (parseInt(currentCell) % 20 === 0) {
+    if (parseInt(currentCell.id) % 20 === 0) {
         console.log('Right Hit');
+        edge = true;
     }
-    if (parseInt(currentCell) - 20 < 1) {
+    if (parseInt(currentCell.id) - 20 < 1) {
         console.log('Top Edge');
+        edge = true
     }
-    if (parseInt(currentCell) + 20 > 400) {
+    if (parseInt(currentCell.id) + 20 > 400) {
         console.log('Bottom Edge');
+        edge = true;
     }
 }
 
 function checkTop(e) {
-    console.log(e.target)
-    currentCell = e.target.getAttribute('id');
+
+    currentCell = e.id;
     console.log(currentCell);
 
     //Top Neighbor
@@ -100,7 +131,7 @@ function checkTop(e) {
 function checkBottom(e) {
 
 
-    currentCell = e.target.getAttribute('id');
+    currentCell = e.id;
     cells.bottomMid = parseInt(currentCell) + 20;
     cells.bottomLeft = parseInt(currentCell) + 19;
     cells.bottomRight = parseInt(currentCell) + 21;
@@ -111,7 +142,7 @@ function checkBottom(e) {
 }
 
 function checkMid(e) {
-    //currentCell = e.target.getAttribute('id');
+    currentCell = e.id;
     cells.leftCenter = parseInt(currentCell) - 1;
     cells.rightCenter = parseInt(currentCell) + 1;
 
@@ -121,8 +152,8 @@ function checkMid(e) {
 
 let bombs = [];
 function generateBombs() {
-    for (let i = 0; i <= 15; i++) {
-        bombs.push(Math.floor(Math.random() * 20 + 1));
+    for (let i = 0; i <= 50; i++) {
+        bombs.push(Math.floor(Math.random() * 400 + 1));
     }
     //  console.log(bombs)
 }
